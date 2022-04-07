92c60ea0008d304935ff01b5a83c43ad97d87cff
zeon77 <2716313+zeon77@users.noreply.github.com>
2022-04-07 15:16:53 +0200

    megoldás 1-2-3.

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-let szamol = function () {
-  let F = document.getElementById('fogyasztas').value;
-  let V = document.getElementById('tank').value;
-  let Ut = document.getElementById('ut').value;
+// -----------------------------------------------------
+// 1. megoldás
+let szamolas1 = function() {
+  let F = document.getElementById('fogyasztas1').value;
+  let V = document.getElementById('tank1').value;
+  let Ut = document.getElementById('ut1').value;
 
   let megtehetoUt = V * 100 / F;
 
-
   if (megtehetoUt < Ut) {
-    document.getElementById('megoldas').innerHTML = "Az út során tankolni kell!";
+    document.getElementById('megoldas1').innerHTML = "Az út során tankolni kell!";
   } else {
-    document.getElementById('megoldas').innerHTML = "Az út megtehető tankolás nélkül."
+    document.getElementById('megoldas1').innerHTML = "Az út megtehető tankolás nélkül."
   }
+
+  return false;
 }
 
-document.getElementById('szamol').addEventListener('click', szamol);
- 
+// -----------------------------------------------------
+// 2. megoldás
+let szamolas2 = function(e) {
+  e.preventDefault();
+  
+  let F = document.getElementById('fogyasztas2').value;
+  let V = document.getElementById('tank2').value;
+  let Ut = document.getElementById('ut2').value;
+
+  let megtehetoUt = V * 100 / F;
+
+  if (megtehetoUt < Ut) {
+    document.getElementById('megoldas2').innerHTML = "Az út során tankolni kell!";
+  } else {
+    document.getElementById('megoldas2').innerHTML = "Az út megtehető tankolás nélkül."
+  }
+}
+document.getElementById('szamolas2').addEventListener('click', szamolas2);
+
+// -----------------------------------------------------
+// 3. megoldás
+let szamolas3 = function() {
+  let F = document.getElementById('fogyasztas3').value;
+  let V = document.getElementById('tank3').value;
+  let Ut = document.getElementById('ut3').value;
+
+  let megtehetoUt = V * 100 / F;
+
+  if (megtehetoUt < Ut) {
+    document.getElementById('megoldas3').innerHTML = "Az út során tankolni kell!";
+  } else {
+    document.getElementById('megoldas3').innerHTML = "Az út megtehető tankolás nélkül."
+  }
+}
+document.getElementById('szamolas3').addEventListener('click', szamolas3);
+// -----------------------------------------------------
